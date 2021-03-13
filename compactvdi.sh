928485124b07db604f0a6e4e26ed024e134ded0e
Patrik Nilsson <nipatriknilsson@gmail.com>
2021-03-13 17:53:41 +0100

    Changed tabs to spaces; dir searched for *.vdi

--- compactvdi.sh
+++ compactvdi.sh
@@ -2,27 +2,27 @@
 
 function compact()
 {
-	local r=1
-	
-	filestamp="$1.stamp"
-	file="$1"
-	
-	if [ -f "$filestamp" ]; then
-		c1=$(stat -c '%Y' "$file")
-		c2=$(stat -c '%Y' "$filestamp")
-		[ "$c1" -eq "$c2" ] && r=0
-	fi
+    local r=1
+    
+    filestamp="$1.stamp"
+    file="$1"
+    
+    if [ -f "$filestamp" ] ; then
+        c1=$(stat -c '%Y' "$file")
+        c2=$(stat -c '%Y' "$filestamp")
+        [ "$c1" -eq "$c2" ] && r=0
+    fi
 
-	echo "$file"
+    echo "$file"
 
-	if [ "$r" == "1" ]; then
-		( VBoxManage showhdinfo "$file" | grep -q -E 'State:[[:space:]]+locked' && echo "Disk file is locked!" ) || ( VBoxManage modifymedium "$file" --compact && touch -r "$file" "$filestamp" || echo "Failed!" )
-	else
-		echo "Already compacted!"
-	fi
+    if [ "$r" == "1" ] ; then
+        ( VBoxManage showhdinfo "$file" | grep -q -E 'State:[[:space:]]+locked' && echo "Disk file is locked!" ) || ( VBoxManage modifymedium "$file" --compact && touch -r "$file" "$filestamp" || echo "Failed!" )
+    else
+        echo "Already compacted!"
+    fi
 
-	echo
+    echo
 }
 
-find /home/ -name '*.vdi' -type f 2>/dev/null | LANG=C sort | while read file; do compact "$file"; done
+find "/home/$USER/VirtualBox VMs" -name '*.vdi' -type f 2>/dev/null | LANG=C sort | while read file; do compact "$file"; done
 
